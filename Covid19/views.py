1ba96ba3f561faaf33875539d02e73d220aab7fc
Vijay <vijaymalviya320@gmail.com>
2020-07-18 01:17:33 +0530

    second commit

--- Covid19/views.py
+++ Covid19/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+import requests
+import bs4
+
+def index(req):
+    url = "http://www.mohfw.gov.in/"
+    data = requests.get(url)
+    bs = bs4.BeautifulSoup(data.text,'html.parser')
+    info = bs.find("div",class_="site-stats-count")
+    print(info)
+    return render(req,'index.html')
+    
+
+	
+
+
+
 
 # Create your views here.
